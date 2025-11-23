112fdf05d2fb755e679a8e228cdcca88415c8708
R Brook <rlbrook@gmail.com>
2025-11-23 17:03:11 +0000

    Initial Next.js setup.

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head"
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>NFL Stats</title>
+      </Head>
+      <main>
+        <div>
+          <h1>NFL Stats</h1>
+        </div>
+      </main>
+    </div>
+  )
+}
